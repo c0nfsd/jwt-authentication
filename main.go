4564103f054cda71059dee09fa4c38b73a316f4c
c0nfsd <pathakmilindd@gmail.com>
2022-08-16 22:27:58 +0530

    initializers

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/c0nfsd/jwt-authentication/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
-	fmt.Println("hello")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
 }
